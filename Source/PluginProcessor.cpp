871dcb7fb74ea03046f042527ae232852630c9b9
Alex Birch <Birch-san@users.noreply.github.com>
2022-01-01 22:16:29 +0000

    zero out incoming audio buffer (this has become necessary for AU plugins)

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -171,6 +171,15 @@ AudioProcessor::BusesProperties JuicySFAudioProcessor::getBusesProperties() {
 void JuicySFAudioProcessor::processBlock(AudioBuffer<float>& buffer, MidiBuffer& midiMessages) {
     jassert (!isUsingDoublePrecision());
 
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (int i = getTotalNumInputChannels(); i < getTotalNumOutputChannels(); ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
     // Now pass any incoming midi messages to our keyboard state object, and let it
     // add messages to the buffer if the user is clicking on the on-screen keys
     keyboardState.processNextMidiBuffer(midiMessages, 0, buffer.getNumSamples(), true);
@@ -183,15 +192,6 @@ void JuicySFAudioProcessor::processBlock(AudioBuffer<float>& buffer, MidiBuffer&
     // (see juce_VST3_Wrapper.cpp for the assertion this would trip otherwise)
     // we are !JucePlugin_ProducesMidiOutput, so clear remaining MIDI messages from our buffer
     midiMessages.clear();
-
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-//    for (int i = getTotalNumInputChannels(); i < getTotalNumOutputChannels(); ++i)
-//        buffer.clear (i, 0, numSamples);
 }
 
 //==============================================================================
