64ba2046e231dc0a9868aee8c4c0a6643e590675
Alex Birch <Birch-san@users.noreply.github.com>
2022-01-08 23:49:27 +0000

    stop utilising pkg-config's "libs other" advice. there were only two flags there (location of libomp, and -pthread). neither helped us (we've now eliminated libomp because OpenMP doesn't support static linking, and the -pthread flag attempted a dynamic link anyway). use CMAKE_EXE_LINKER_FLAGS to pass in winpthread library. delete out-of-date sandboxing pessimism (since I already implemented security-scoped tokens).

--- win32_cross_compile/configure_juicysfplugin.sh
+++ win32_cross_compile/configure_juicysfplugin.sh
@@ -14,17 +14,23 @@ for ARCH in ${ARCHS[@]}; do
   TOOLCHAIN="${TOOLCHAINS[$ARCH]}"
   echo "toolchain: $TOOLCHAIN"
   TOOLCHAIN_FILE="/build/${TOOLCHAIN}_toolchain.cmake"
+  TOOLCHAIN_LIB_DIR="/opt/llvm-mingw/${TOOLCHAIN}-w64-mingw32/lib"
   echo "toolchain file: $TOOLCHAIN_FILE"
 
   BUILD="build_$ARCH"
 
   # we don't have pkg-config files for system libraries (e.g. libintl),
   # so pkg-config doesn't know that it depends on libiconv.
-  # hence we introduce via CMAKE_EXE_LINKER_FLAGS.
+  # hence we introduce libiconv via CMAKE_EXE_LINKER_FLAGS.
+
+  # the advice in pkg-config "libs other" (to provide -pthread flag to linker)
+  # ends up dynamically linking it, even if we preface with -Wl,Bstatic
+  # so I've told CMakeFiles not to use the "libs other" advice.
+  # consequentially it's up to us to link in a suitable pthread archive via CMAKE_EXE_LINKER_FLAGS
   VERBOSE=1 PKG_CONFIG_PATH="/$REPO/lib/pkgconfig" cmake -B"$BUILD" \
 -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
 -DCMAKE_PREFIX_PATH="/linux_native" \
--DCMAKE_EXE_LINKER_FLAGS="/$REPO/lib/libiconv.a" \
+-DCMAKE_EXE_LINKER_FLAGS="/$REPO/lib/libiconv.a $TOOLCHAIN_LIB_DIR/libwinpthread.a" \
 -DCMAKE_INSTALL_PREFIX="/$REPO" \
 -DVST2_SDK_PATH="/VST2_SDK" \
 -DCMAKE_CXX_FLAGS='-DJUCE_DISABLE_ASSERTIONS' \
